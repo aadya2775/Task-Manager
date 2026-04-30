00cb898ce9d8f92dd394ad90d17a2bdb67397fdf
aadya2775 <aadya2775@gmail.com>
2026-04-30 21:16:57 +0530

    axiosInstance

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://task-manager-01.up.railway.app/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
