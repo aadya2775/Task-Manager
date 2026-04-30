a98507edeaf40d8d650e85024e807a5cd75b7353
aadya2775 <aadya2775@gmail.com>
2026-04-30 23:07:11 +0530

    axios

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from "axios"
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
@@ -10,16 +10,6 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
     Accept: "application/json",
   },
-});
+})
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-export default axiosInstance;
+export default axiosInstance
